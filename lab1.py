3b45bfd07458009fa8227e3c09e00eb0596ff5f0
yb705 <59323484+yb705@users.noreply.github.com>
2020-01-10 22:09:51 +0800

    Rename lab1.py to 数字列表生成.py

--- lab1.py
+++
@@ -1,8 +0,0 @@
-number1=[]
-for number in range(1,10):
-	number1.append(number)#生成列表的法一
-print(number1)
-number2=list(range(1,10))#生成列表的法二，列表解析
-print(number2)
-number3=[number for number in range(1,10)]#生成列表的法三
-print(number3)
