41bde3b0aeeb5b9b8f3feddde994e84f3c176c12
yb705 <59323484+yb705@users.noreply.github.com>
2020-01-16 14:49:43 +0800

    Update 数字列表生成.py

--- 数字列表生成.py
+++ 数字列表生成.py
@@ -2,7 +2,7 @@ number1=[]
 for number in range(1,10):
 	number1.append(number)#生成列表的法一
 print(number1)
-number2=list(range(1,10))#生成列表的法二，列表解析
+number2=list(range(1,10))#生成列表的法二
 print(number2)
-number3=[number for number in range(1,10)]#生成列表的法三
+number3=[number for number in range(1,10)]#生成列表的法三，列表解析
 print(number3)
