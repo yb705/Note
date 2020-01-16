89538bfb0be665cd40b8e265c714ab8ca5cfb70a
yb705 <59323484+yb705@users.noreply.github.com>
2020-01-16 14:48:04 +0800

    Create 列表与字典遍历.py

---
+++ 列表与字典遍历.py
@@ -0,0 +1,4 @@
+列表遍历：for value in name_of_list
+字典遍历：for key,value in name_of_dictionary.items()#键—值遍历
+         for key in name_of_dictionary.keys()#键遍历
+         for value in name_of_dictionary.values()#值遍历
