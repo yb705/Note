02e9b8766730780e208f5ecfbf3b71abc8546ca2
yb705 <59323484+yb705@users.noreply.github.com>
2020-01-17 20:15:57 +0800

    Update note.py

--- note.py
+++ note.py
@@ -1,2 +1,3 @@
-注意列表与字典的引用都是用[],如message=['a','b','c'];print(message[2])
+注意列表与字典还有元组的引用都是用[],如message=['a','b','c'];print(message[2])
 message={'a','b','c'};print(message[2])
+message=（'a','b','c'）;print(message[2])
