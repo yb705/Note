273ef8fec4f685dac85ead9915338c0f1cec4342
yb705 <59323484+yb705@users.noreply.github.com>
2020-04-03 16:49:44 +0800

    Update 数字列表生成.py

--- 数字列表生成.py
+++ 数字列表生成.py
@@ -4,5 +4,5 @@ for number in range(1,10):
 print(number1)
 number2=list(range(1,10))#生成列表的法二
 print(number2)
-number3=[number for number in range(1,10)]#生成列表的法三，列表解析
+number3=[number**2 for number in range(1,10)]#生成列表的法三，列表解析
 print(number3)
